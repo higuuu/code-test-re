4213a052074ca92050d45877a38ddfd54b413f82
higuchi shuya <higuchi_s@mti.co.jp>
2020-07-17 16:58:17 +0900

    [mod] これでボタンを押したら反映させることができる

--- node/index.js
+++ node/index.js
@@ -1,18 +1,19 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res)=>{
-   const endpoint = req.url;
-   if( endpoint==='/start' ){
-      fs.readFile('./index.html',(err, data)=>{
-         res.writeHead(200, {'Content-Type': 'text/html'});
-         res.write(data);
-         res.end();
-      });
-   }
-   if( endpoint==='/api' ){
-      // ここに処理を記述してください。
-      res.end({'data':'Hello World!!'})
-   }
+const server = http.createServer((req, res) => {
+  const endpoint = req.url;
+  if (endpoint === "/start") {
+    fs.readFile("./index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+  if (endpoint === "/api") {
+    // ここに処理を記述してください。
+    res.statusCode = 200;
+    res.send('{ "data": "Hello World" }');
+  }
 });
-server.listen(8080); 
+server.listen(8080);
